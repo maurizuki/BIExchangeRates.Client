2523216fa98850bc5a3880c511fa8a7b40ca9e07
maurizuki <62178459+maurizuki@users.noreply.github.com>
2020-10-21 22:00:53 +0200

    Updated console application

--- BIExchangeRates.Console/Verbs/DailyRates.cs
+++ BIExchangeRates.Console/Verbs/DailyRates.cs
@@ -1,4 +1,27 @@
-﻿using BIExchangeRates.Client;
+﻿// Copyright (c) 2020 Maurizio Basaglia
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+
+using BIExchangeRates.Client;
 using CommandLine;
 using System;
 using System.Collections.Generic;
@@ -21,9 +44,8 @@ namespace BIExchangeRates.Console.Verbs
 			HelpText = "List of ISO codes of the required currencies. Leave empty to get all the valid currencies.")]
 		public IEnumerable<string> BaseCurrencyIsoCodes { get; set; }
 
-		public static void Execute(DailyRates options)
+		public static void Execute(IExchangeRatesClient client, DailyRates options)
 		{
-			var client = new ExchangeRatesClient();
 			var model = client.GetDailyRates(options.ReferenceDate,
 				options.BaseCurrencyIsoCodes.Select(e => e.ToUpper()),
 				options.CurrencyIsoCode.ToUpper()).Result;
--- BIExchangeRates.Console/Verbs/LatestRates.cs
+++ BIExchangeRates.Console/Verbs/LatestRates.cs
@@ -1,4 +1,27 @@
-﻿using BIExchangeRates.Client;
+﻿// Copyright (c) 2020 Maurizio Basaglia
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+
+using BIExchangeRates.Client;
 using CommandLine;
 using System.Linq;
 
@@ -7,9 +30,8 @@ namespace BIExchangeRates.Console.Verbs
     [Verb("latest", HelpText = "Latest available exchange rates for all the valid currencies.")]
 	public class LatestRates
     {
-		public static void Execute(LatestRates options)
+		public static void Execute(IExchangeRatesClient client)
 		{
-			var client = new ExchangeRatesClient();
 			var model = client.GetLatestRates().Result;
 			var usdExchangeConventions = model.LatestRates.Where(e => e.IsoCode != "USD")
 				.Select(e => new { e.UsdExchangeConventionCode, e.UsdExchangeConvention }).Distinct().ToList();
