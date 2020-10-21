19dcf34aadecdac1de5e8d2337a87b022690f4f7
maurizuki <62178459+maurizuki@users.noreply.github.com>
2020-10-21 21:46:20 +0200

    Update XML documentation

--- BIExchangeRates.Client/ExchangeRatesClient.cs
+++ BIExchangeRates.Client/ExchangeRatesClient.cs
@@ -34,7 +34,7 @@ using System.Threading.Tasks;
 namespace BIExchangeRates.Client
 {
 	/// <summary>
-	/// A wrapper for the REST API of the currency exchange rates of Banca d'Italia (https://tassidicambio.bancaditalia.it).
+	/// Provides a wrapper for the REST API of the currency exchange rates of Banca d'Italia (https://tassidicambio.bancaditalia.it).
 	/// </summary>
 	public sealed class ExchangeRatesClient : IExchangeRatesClient
 	{
@@ -71,35 +71,36 @@ namespace BIExchangeRates.Client
 		/// Returns the latest available exchange rates for all the valid currencies.
 		/// </summary>
 		/// <param name="language">The language of the returned data.</param>
-		/// <returns></returns>
+		/// <returns>A task that represents the asynchronous operation. The task result contains the latest available exchange rates for all the valid currencies.</returns>
 		/// <exception cref="HttpRequestException"></exception>
 		public async Task<LatestRatesModel> GetLatestRates(Language language = Language.En) =>
 			JsonConvert.DeserializeObject<LatestRatesModel>(
 				await GetAsync($"{LatestRatesPath}?lang={language}"));
 
 		/// <summary>
-		/// Returns the daily exchange rates for a specific date.
+		/// Returns the daily exchange rates for a specific date for all the available currencies.
 		/// </summary>
 		/// <param name="referenceDate">The reference date for the exchange rates.</param>
-		/// <param name="currencyIsoCode">The ISO code of the reference currency ("EUR" or "USD", case insensitive).</param>
+		/// <param name="currencyIsoCode">The ISO code of the reference currency ("EUR", "USD" or "ITL", case insensitive).</param>
 		/// <param name="language">The language of the returned data.</param>
-		/// <returns></returns>
+		/// <returns>A task that represents the asynchronous operation. The task result contains the daily exchange rates for a specific date for all the available currencies.</returns>
 		/// <exception cref="HttpRequestException"></exception>
 		public async Task<DailyRatesModel> GetDailyRates(DateTime referenceDate, string currencyIsoCode,
 			Language language = Language.En) =>
 			await GetDailyRates(referenceDate, new List<string>(), currencyIsoCode, language);
 
 		/// <summary>
-		/// Returns the daily exchange rates for a specific date.
+		/// Returns the daily exchange rates for a specific date for a list of currencies.
 		/// </summary>
 		/// <param name="referenceDate">The reference date for the exchange rates.</param>
 		/// <param name="baseCurrencyIsoCodes">The list of ISO codes of the required currencies (case insensitive).</param>
-		/// <param name="currencyIsoCode">The ISO code of the reference currency ("EUR" or "USD", case insensitive).</param>
+		/// <param name="currencyIsoCode">The ISO code of the reference currency ("EUR", "USD" or "ITL", case insensitive).</param>
 		/// <param name="language">The language of the returned data.</param>
-		/// <returns></returns>
+		/// <returns>A task that represents the asynchronous operation. The task result contains the daily exchange rates for a specific date for a list of currencies.</returns>
+		/// <exception cref="ArgumentNullException"></exception>
 		/// <exception cref="HttpRequestException"></exception>
-		public async Task<DailyRatesModel> GetDailyRates(DateTime referenceDate,
-			IEnumerable<string> baseCurrencyIsoCodes, string currencyIsoCode, Language language = Language.En)
+		public async Task<DailyRatesModel> GetDailyRates(DateTime referenceDate, IEnumerable<string> baseCurrencyIsoCodes,
+			string currencyIsoCode, Language language = Language.En)
 		{
 			if (baseCurrencyIsoCodes is null) throw new ArgumentNullException(nameof(baseCurrencyIsoCodes));
 			var baseCurrencyIsoCodeList = baseCurrencyIsoCodes.ToList();
@@ -114,27 +115,28 @@ namespace BIExchangeRates.Client
 		}
 
 		/// <summary>
-		/// Returns the monthly average exchange rates for specific month and year.
+		/// Returns the monthly average exchange rates for specific month and year for all the available currencies.
 		/// </summary>
 		/// <param name="month">The reference month for the exchange rates (1-12).</param>
 		/// <param name="year">The reference year for the exchange rates.</param>
-		/// <param name="currencyIsoCode">The ISO code of the reference currency ("EUR" or "USD", case insensitive).</param>
+		/// <param name="currencyIsoCode">The ISO code of the reference currency ("EUR", "USD" or "ITL", case insensitive).</param>
 		/// <param name="language">The language of the returned data.</param>
-		/// <returns></returns>
+		/// <returns>A task that represents the asynchronous operation. The task result contains the monthly average exchange rates for specific month and year for all the available currencies.</returns>
 		/// <exception cref="HttpRequestException"></exception>
 		public async Task<MonthlyAverageRatesModel> GetMonthlyAverageRates(int month, int year, string currencyIsoCode,
 			Language language = Language.En) =>
 			await GetMonthlyAverageRates(month, year, new List<string>(), currencyIsoCode, language);
 
 		/// <summary>
-		/// Returns the monthly average exchange rates for specific month and year.
+		/// Returns the monthly average exchange rates for specific month and year for a list of currencies.
 		/// </summary>
 		/// <param name="month">The reference month for the exchange rates (1-12).</param>
 		/// <param name="year">The reference year for the exchange rates.</param>
 		/// <param name="baseCurrencyIsoCodes">The list of ISO codes of the required currencies (case insensitive).</param>
-		/// <param name="currencyIsoCode">The ISO code of the reference currency ("EUR" or "USD", case insensitive).</param>
+		/// <param name="currencyIsoCode">The ISO code of the reference currency ("EUR", "USD" or "ITL", case insensitive).</param>
 		/// <param name="language">The language of the returned data.</param>
-		/// <returns></returns>
+		/// <returns>A task that represents the asynchronous operation. The task result contains the monthly average exchange rates for specific month and year for a list of currencies.</returns>
+		/// <exception cref="ArgumentNullException"></exception>
 		/// <exception cref="HttpRequestException"></exception>
 		public async Task<MonthlyAverageRatesModel> GetMonthlyAverageRates(int month, int year,
 			IEnumerable<string> baseCurrencyIsoCodes, string currencyIsoCode, Language language = Language.En)
@@ -153,29 +155,29 @@ namespace BIExchangeRates.Client
 		}
 
 		/// <summary>
-		/// Returns the annual average exchange rates for a specific year.
+		/// Returns the annual average exchange rates for a specific year for all the available currencies.
 		/// </summary>
 		/// <param name="year">The reference year for the exchange rates.</param>
-		/// <param name="currencyIsoCode">The ISO code of the reference currency ("EUR" or "USD", case insensitive).</param>
+		/// <param name="currencyIsoCode">The ISO code of the reference currency ("EUR", "USD" or "ITL", case insensitive).</param>
 		/// <param name="language">The language of the returned data.</param>
-		/// <returns></returns>
+		/// <returns>A task that represents the asynchronous operation. The task result contains the annual average exchange rates for a specific year for all the available currencies.</returns>
 		/// <exception cref="HttpRequestException"></exception>
 		public async Task<AnnualAverageRatesModel> GetAnnualAverageRates(int year, string currencyIsoCode,
 			Language language = Language.En) =>
 			await GetAnnualAverageRates(year, new List<string>(), currencyIsoCode, language);
 
 		/// <summary>
-		/// Returns the annual average exchange rates for a specific year.
+		/// Returns the annual average exchange rates for a specific year for a list of currencies.
 		/// </summary>
 		/// <param name="year">The reference year for the exchange rates.</param>
 		/// <param name="baseCurrencyIsoCodes">The list of ISO codes of the required currencies (case insensitive).</param>
-		/// <param name="currencyIsoCode">The ISO code of the reference currency ("EUR" or "USD", case insensitive).</param>
+		/// <param name="currencyIsoCode">The ISO code of the reference currency ("EUR", "USD" or "ITL", case insensitive).</param>
 		/// <param name="language">The language of the returned data.</param>
-		/// <returns></returns>
+		/// <returns>A task that represents the asynchronous operation. The task result contains the annual average exchange rates for a specific year for a list of currencies.</returns>
+		/// <exception cref="ArgumentNullException"></exception>
 		/// <exception cref="HttpRequestException"></exception>
 		public async Task<AnnualAverageRatesModel> GetAnnualAverageRates(int year,
 			IEnumerable<string> baseCurrencyIsoCodes, string currencyIsoCode, Language language = Language.En)
-
 		{
 			if (baseCurrencyIsoCodes is null) throw new ArgumentNullException(nameof(baseCurrencyIsoCodes));
 			var baseCurrencyIsoCodeList = baseCurrencyIsoCodes.ToList();
@@ -195,9 +197,9 @@ namespace BIExchangeRates.Client
 		/// <param name="startDate">The start date of the range for the exchange rates.</param>
 		/// <param name="endDate">The end date of the range for the exchange rates.</param>
 		/// <param name="baseCurrencyIsoCode">The ISO code of the required currency (case insensitive).</param>
-		/// <param name="currencyIsoCode">The ISO code of the reference currency ("EUR" or "USD", case insensitive).</param>
+		/// <param name="currencyIsoCode">The ISO code of the reference currency ("EUR", "USD" or "ITL", case insensitive).</param>
 		/// <param name="language">The language of the returned data.</param>
-		/// <returns></returns>
+		/// <returns>A task that represents the asynchronous operation. The task result contains the daily exchange rates of a currency for a specific date range.</returns>
 		/// <exception cref="HttpRequestException"></exception>
 		public async Task<DailyTimeSeriesModel> GetDailyTimeSeries(DateTime startDate, DateTime endDate,
 			string baseCurrencyIsoCode, string currencyIsoCode, Language language = Language.En) =>
@@ -218,9 +220,9 @@ namespace BIExchangeRates.Client
 		/// <param name="endMonth">The end month of the range for the exchange rates (1-12).</param>
 		/// <param name="endYear">The end year of the range for the exchange rates.</param>
 		/// <param name="baseCurrencyIsoCode">The ISO code of the required currency (case insensitive).</param>
-		/// <param name="currencyIsoCode">The ISO code of the reference currency ("EUR" or "USD", case insensitive).</param>
+		/// <param name="currencyIsoCode">The ISO code of the reference currency ("EUR", "USD" or "ITL", case insensitive).</param>
 		/// <param name="language">The language of the returned data.</param>
-		/// <returns></returns>
+		/// <returns>A task that represents the asynchronous operation. The task result contains the monthly average exchange rates of a currency for a specific month range.</returns>
 		/// <exception cref="HttpRequestException"></exception>
 		public async Task<MonthlyTimeSeriesModel> GetMonthlyTimeSeries(int startMonth, int startYear, int endMonth, int endYear,
 			string baseCurrencyIsoCode, string currencyIsoCode, Language language = Language.En) =>
@@ -241,12 +243,12 @@ namespace BIExchangeRates.Client
 		/// <param name="startYear">The start year of the range for the exchange rates.</param>
 		/// <param name="endYear">The end year of the range for the exchange rates.</param>
 		/// <param name="baseCurrencyIsoCode">The ISO code of the required currency (case insensitive).</param>
-		/// <param name="currencyIsoCode">The ISO code of the reference currency ("EUR" or "USD", case insensitive).</param>
+		/// <param name="currencyIsoCode">The ISO code of the reference currency ("EUR", "USD" or "ITL", case insensitive).</param>
 		/// <param name="language">The language of the returned data.</param>
-		/// <returns></returns>
+		/// <returns>A task that represents the asynchronous operation. The task result contains the annual average exchange rates of a currency for a specific year range.</returns>
 		/// <exception cref="HttpRequestException"></exception>
-		public async Task<AnnualTimeSeriesModel> GetAnnualTimeSeries(int startYear, int endYear,
-			string baseCurrencyIsoCode, string currencyIsoCode, Language language = Language.En) =>
+		public async Task<AnnualTimeSeriesModel> GetAnnualTimeSeries(int startYear, int endYear, string baseCurrencyIsoCode,
+			string currencyIsoCode, Language language = Language.En) =>
 			JsonConvert.DeserializeObject<AnnualTimeSeriesModel>(
 				await GetAsync(new StringBuilder(AnnualTimeSeriesPath)
 					.Append("?startYear=").Append(startYear)
@@ -260,7 +262,7 @@ namespace BIExchangeRates.Client
 		/// Returns the list of all the available currencies.
 		/// </summary>
 		/// <param name="language">The language of the returned data.</param>
-		/// <returns></returns>
+		/// <returns>A task that represents the asynchronous operation. The task result contains the list of all the available currencies.</returns>
 		/// <exception cref="HttpRequestException"></exception>
 		public async Task<CurrenciesModel> GetCurrencies(Language language = Language.En) =>
 			JsonConvert.DeserializeObject<CurrenciesModel>(
--- BIExchangeRates.Client/IExchangeRatesClient.cs
+++ BIExchangeRates.Client/IExchangeRatesClient.cs
@@ -30,35 +30,124 @@ namespace BIExchangeRates.Client
 {
 	public enum Language { En, It }
 
+	/// <summary>
+	/// Defines a wrapper for the REST API of the currency exchange rates of Banca d'Italia (https://tassidicambio.bancaditalia.it).
+	/// </summary>
 	public interface IExchangeRatesClient
 	{
+		/// <summary>
+		/// Returns the latest available exchange rates for all the valid currencies.
+		/// </summary>
+		/// <param name="language">The language of the returned data.</param>
+		/// <returns>A task that represents the asynchronous operation. The task result contains the latest available exchange rates for all the valid currencies.</returns>
 		Task<LatestRatesModel> GetLatestRates(Language language = Language.En);
 
+		/// <summary>
+		/// Returns the daily exchange rates for a specific date for all the available currencies.
+		/// </summary>
+		/// <param name="referenceDate">The reference date for the exchange rates.</param>
+		/// <param name="currencyIsoCode">The ISO code of the reference currency ("EUR", "USD" or "ITL", case insensitive).</param>
+		/// <param name="language">The language of the returned data.</param>
+		/// <returns>A task that represents the asynchronous operation. The task result contains the daily exchange rates for a specific date for all the available currencies.</returns>
 		Task<DailyRatesModel> GetDailyRates(DateTime referenceDate, string currencyIsoCode, Language language = Language.En);
 
+		/// <summary>
+		/// Returns the daily exchange rates for a specific date for a list of currencies.
+		/// </summary>
+		/// <param name="referenceDate">The reference date for the exchange rates.</param>
+		/// <param name="baseCurrencyIsoCodes">The list of ISO codes of the required currencies (case insensitive).</param>
+		/// <param name="currencyIsoCode">The ISO code of the reference currency ("EUR", "USD" or "ITL", case insensitive).</param>
+		/// <param name="language">The language of the returned data.</param>
+		/// <returns>A task that represents the asynchronous operation. The task result contains the daily exchange rates for a specific date for a list of currencies.</returns>
 		Task<DailyRatesModel> GetDailyRates(DateTime referenceDate, IEnumerable<string> baseCurrencyIsoCodes,
 			string currencyIsoCode, Language language = Language.En);
 
+		/// <summary>
+		/// Returns the monthly average exchange rates for specific month and year for all the available currencies.
+		/// </summary>
+		/// <param name="month">The reference month for the exchange rates (1-12).</param>
+		/// <param name="year">The reference year for the exchange rates.</param>
+		/// <param name="currencyIsoCode">The ISO code of the reference currency ("EUR", "USD" or "ITL", case insensitive).</param>
+		/// <param name="language">The language of the returned data.</param>
+		/// <returns>A task that represents the asynchronous operation. The task result contains the monthly average exchange rates for specific month and year for all the available currencies.</returns>
 		Task<MonthlyAverageRatesModel> GetMonthlyAverageRates(int month, int year, string currencyIsoCode,
 			Language language = Language.En);
 
+		/// <summary>
+		/// Returns the monthly average exchange rates for specific month and year for a list of currencies.
+		/// </summary>
+		/// <param name="month">The reference month for the exchange rates (1-12).</param>
+		/// <param name="year">The reference year for the exchange rates.</param>
+		/// <param name="baseCurrencyIsoCodes">The list of ISO codes of the required currencies (case insensitive).</param>
+		/// <param name="currencyIsoCode">The ISO code of the reference currency ("EUR", "USD" or "ITL", case insensitive).</param>
+		/// <param name="language">The language of the returned data.</param>
+		/// <returns>A task that represents the asynchronous operation. The task result contains the monthly average exchange rates for specific month and year for a list of currencies.</returns>
 		Task<MonthlyAverageRatesModel> GetMonthlyAverageRates(int month, int year, IEnumerable<string> baseCurrencyIsoCodes,
 			string currencyIsoCode, Language language = Language.En);
 
+		/// <summary>
+		/// Returns the annual average exchange rates for a specific year for all the available currencies.
+		/// </summary>
+		/// <param name="year">The reference year for the exchange rates.</param>
+		/// <param name="currencyIsoCode">The ISO code of the reference currency ("EUR", "USD" or "ITL", case insensitive).</param>
+		/// <param name="language">The language of the returned data.</param>
+		/// <returns>A task that represents the asynchronous operation. The task result contains the annual average exchange rates for a specific year for all the available currencies.</returns>
 		Task<AnnualAverageRatesModel> GetAnnualAverageRates(int year, string currencyIsoCode, Language language = Language.En);
 
+		/// <summary>
+		/// Returns the annual average exchange rates for a specific year for a list of currencies.
+		/// </summary>
+		/// <param name="year">The reference year for the exchange rates.</param>
+		/// <param name="baseCurrencyIsoCodes">The list of ISO codes of the required currencies (case insensitive).</param>
+		/// <param name="currencyIsoCode">The ISO code of the reference currency ("EUR", "USD" or "ITL", case insensitive).</param>
+		/// <param name="language">The language of the returned data.</param>
+		/// <returns>A task that represents the asynchronous operation. The task result contains the annual average exchange rates for a specific year for a list of currencies.</returns>
 		Task<AnnualAverageRatesModel> GetAnnualAverageRates(int year, IEnumerable<string> baseCurrencyIsoCodes,
 			string currencyIsoCode, Language language = Language.En);
 
+		/// <summary>
+		/// Returns the daily exchange rates of a currency for a specific date range.
+		/// </summary>
+		/// <param name="startDate">The start date of the range for the exchange rates.</param>
+		/// <param name="endDate">The end date of the range for the exchange rates.</param>
+		/// <param name="baseCurrencyIsoCode">The ISO code of the required currency (case insensitive).</param>
+		/// <param name="currencyIsoCode">The ISO code of the reference currency ("EUR", "USD" or "ITL", case insensitive).</param>
+		/// <param name="language">The language of the returned data.</param>
+		/// <returns>A task that represents the asynchronous operation. The task result contains the daily exchange rates of a currency for a specific date range.</returns>
 		Task<DailyTimeSeriesModel> GetDailyTimeSeries(DateTime startDate, DateTime endDate, string baseCurrencyIsoCode,
 			string currencyIsoCode, Language language = Language.En);
 
+		/// <summary>
+		/// Returns the monthly average exchange rates of a currency for a specific month range.
+		/// </summary>
+		/// <param name="startMonth">The start month of the range for the exchange rates (1-12).</param>
+		/// <param name="startYear">The start year of the range for the exchange rates.</param>
+		/// <param name="endMonth">The end month of the range for the exchange rates (1-12).</param>
+		/// <param name="endYear">The end year of the range for the exchange rates.</param>
+		/// <param name="baseCurrencyIsoCode">The ISO code of the required currency (case insensitive).</param>
+		/// <param name="currencyIsoCode">The ISO code of the reference currency ("EUR", "USD" or "ITL", case insensitive).</param>
+		/// <param name="language">The language of the returned data.</param>
+		/// <returns>A task that represents the asynchronous operation. The task result contains the monthly average exchange rates of a currency for a specific month range.</returns>
 		Task<MonthlyTimeSeriesModel> GetMonthlyTimeSeries(int startMonth, int startYear, int endMonth, int endYear,
 			string baseCurrencyIsoCode, string currencyIsoCode, Language language = Language.En);
 
+		/// <summary>
+		/// Returns the annual average exchange rates of a currency for a specific year range.
+		/// </summary>
+		/// <param name="startYear">The start year of the range for the exchange rates.</param>
+		/// <param name="endYear">The end year of the range for the exchange rates.</param>
+		/// <param name="baseCurrencyIsoCode">The ISO code of the required currency (case insensitive).</param>
+		/// <param name="currencyIsoCode">The ISO code of the reference currency ("EUR", "USD" or "ITL", case insensitive).</param>
+		/// <param name="language">The language of the returned data.</param>
+		/// <returns>A task that represents the asynchronous operation. The task result contains the annual average exchange rates of a currency for a specific year range.</returns>
 		Task<AnnualTimeSeriesModel> GetAnnualTimeSeries(int startYear, int endYear, string baseCurrencyIsoCode,
 			string currencyIsoCode, Language language = Language.En);
 
+		/// <summary>
+		/// Returns the list of all the available currencies.
+		/// </summary>
+		/// <param name="language">The language of the returned data.</param>
+		/// <returns>A task that represents the asynchronous operation. The task result contains the list of all the available currencies.</returns>
 		Task<CurrenciesModel> GetCurrencies(Language language = Language.En);
 	}
 }
