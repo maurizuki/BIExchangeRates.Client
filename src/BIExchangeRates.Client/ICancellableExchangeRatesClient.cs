038792be66577afe8994000a3698d11777a66c87
Maurizio Basaglia <mauriziobasaglia@hotmail.com>
2025-11-06 15:03:11 +0100

    Add ICancellableExchangeRatesClient

--- src/BIExchangeRates.Client/ICancellableExchangeRatesClient.cs
+++ src/BIExchangeRates.Client/ICancellableExchangeRatesClient.cs
@@ -24,100 +24,105 @@
 using BIExchangeRates.Client.Data;
 using System;
 using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace BIExchangeRates.Client;
 
 /// <summary>
-/// Defines methods to access the REST API of the currency exchange rates of Banca d'Italia (https://tassidicambio.bancaditalia.it).
+/// Defines methods to access the REST API of the currency exchange rates of Banca d'Italia (https://tassidicambio.bancaditalia.it). All methods accept a cancellation token that can be used to receive notice of cancellation.
 /// </summary>
-public interface IExchangeRatesClient
+public interface ICancellableExchangeRatesClient
 {
 	/// <summary>
 	/// Returns the latest available exchange rates for all the valid currencies.
 	/// </summary>
+	/// <param name="cancellationToken">A cancellation token that can be used to receive notice of cancellation.</param>
 	/// <param name="language">The language of the returned data.</param>
 	/// <returns>A task that represents the asynchronous operation. The task result contains the latest available exchange rates for all the valid currencies.</returns>
-	Task<LatestRatesModel> GetLatestRates(Language language = Language.En);
+	Task<LatestRatesModel> GetLatestRates(CancellationToken cancellationToken, Language language = Language.En);
 
 	/// <summary>
 	/// Returns the daily exchange rates for a specific date for all the available currencies.
 	/// </summary>
+	/// <param name="cancellationToken">A cancellation token that can be used to receive notice of cancellation.</param>
 	/// <param name="referenceDate">The reference date for the exchange rates.</param>
 	/// <param name="currencyIsoCode">The ISO 4217 code of the reference currency ("EUR", "USD" or "ITL", case insensitive).</param>
 	/// <param name="language">The language of the returned data.</param>
 	/// <returns>A task that represents the asynchronous operation. The task result contains the daily exchange rates for a specific date for all the available currencies.</returns>
-	Task<DailyRatesModel> GetDailyRates(DateTime referenceDate, string currencyIsoCode, Language language = Language.En);
+	Task<DailyRatesModel> GetDailyRates(CancellationToken cancellationToken, DateTime referenceDate, string currencyIsoCode, Language language = Language.En);
 
 	/// <summary>
 	/// Returns the daily exchange rates for a specific date for a list of currencies.
 	/// </summary>
+	/// <param name="cancellationToken">A cancellation token that can be used to receive notice of cancellation.</param>
 	/// <param name="referenceDate">The reference date for the exchange rates.</param>
 	/// <param name="baseCurrencyIsoCodes">The list of ISO 4217 codes of the required currencies (case insensitive).</param>
 	/// <param name="currencyIsoCode">The ISO 4217 code of the reference currency ("EUR", "USD" or "ITL", case insensitive).</param>
 	/// <param name="language">The language of the returned data.</param>
 	/// <returns>A task that represents the asynchronous operation. The task result contains the daily exchange rates for a specific date for a list of currencies.</returns>
-	Task<DailyRatesModel> GetDailyRates(DateTime referenceDate, IEnumerable<string> baseCurrencyIsoCodes,
-		string currencyIsoCode, Language language = Language.En);
+	Task<DailyRatesModel> GetDailyRates(CancellationToken cancellationToken, DateTime referenceDate, IEnumerable<string> baseCurrencyIsoCodes, string currencyIsoCode, Language language = Language.En);
 
 	/// <summary>
 	/// Returns the monthly average exchange rates for specific month and year for all the available currencies.
 	/// </summary>
+	/// <param name="cancellationToken">A cancellation token that can be used to receive notice of cancellation.</param>
 	/// <param name="month">The reference month for the exchange rates (1-12).</param>
 	/// <param name="year">The reference year for the exchange rates.</param>
 	/// <param name="currencyIsoCode">The ISO 4217 code of the reference currency ("EUR", "USD" or "ITL", case insensitive).</param>
 	/// <param name="language">The language of the returned data.</param>
 	/// <returns>A task that represents the asynchronous operation. The task result contains the monthly average exchange rates for specific month and year for all the available currencies.</returns>
-	Task<MonthlyAverageRatesModel> GetMonthlyAverageRates(int month, int year, string currencyIsoCode,
-		Language language = Language.En);
+	Task<MonthlyAverageRatesModel> GetMonthlyAverageRates(CancellationToken cancellationToken, int month, int year, string currencyIsoCode, Language language = Language.En);
 
 	/// <summary>
 	/// Returns the monthly average exchange rates for specific month and year for a list of currencies.
 	/// </summary>
+	/// <param name="cancellationToken">A cancellation token that can be used to receive notice of cancellation.</param>
 	/// <param name="month">The reference month for the exchange rates (1-12).</param>
 	/// <param name="year">The reference year for the exchange rates.</param>
 	/// <param name="baseCurrencyIsoCodes">The list of ISO 4217 codes of the required currencies (case insensitive).</param>
 	/// <param name="currencyIsoCode">The ISO 4217 code of the reference currency ("EUR", "USD" or "ITL", case insensitive).</param>
 	/// <param name="language">The language of the returned data.</param>
 	/// <returns>A task that represents the asynchronous operation. The task result contains the monthly average exchange rates for specific month and year for a list of currencies.</returns>
-	Task<MonthlyAverageRatesModel> GetMonthlyAverageRates(int month, int year, IEnumerable<string> baseCurrencyIsoCodes,
-		string currencyIsoCode, Language language = Language.En);
+	Task<MonthlyAverageRatesModel> GetMonthlyAverageRates(CancellationToken cancellationToken, int month, int year, IEnumerable<string> baseCurrencyIsoCodes, string currencyIsoCode, Language language = Language.En);
 
 	/// <summary>
 	/// Returns the annual average exchange rates for a specific year for all the available currencies.
 	/// </summary>
+	/// <param name="cancellationToken">A cancellation token that can be used to receive notice of cancellation.</param>
 	/// <param name="year">The reference year for the exchange rates.</param>
 	/// <param name="currencyIsoCode">The ISO 4217 code of the reference currency ("EUR", "USD" or "ITL", case insensitive).</param>
 	/// <param name="language">The language of the returned data.</param>
 	/// <returns>A task that represents the asynchronous operation. The task result contains the annual average exchange rates for a specific year for all the available currencies.</returns>
-	Task<AnnualAverageRatesModel> GetAnnualAverageRates(int year, string currencyIsoCode, Language language = Language.En);
+	Task<AnnualAverageRatesModel> GetAnnualAverageRates(CancellationToken cancellationToken, int year, string currencyIsoCode, Language language = Language.En);
 
 	/// <summary>
 	/// Returns the annual average exchange rates for a specific year for a list of currencies.
 	/// </summary>
+	/// <param name="cancellationToken">A cancellation token that can be used to receive notice of cancellation.</param>
 	/// <param name="year">The reference year for the exchange rates.</param>
 	/// <param name="baseCurrencyIsoCodes">The list of ISO 4217 codes of the required currencies (case insensitive).</param>
 	/// <param name="currencyIsoCode">The ISO 4217 code of the reference currency ("EUR", "USD" or "ITL", case insensitive).</param>
 	/// <param name="language">The language of the returned data.</param>
 	/// <returns>A task that represents the asynchronous operation. The task result contains the annual average exchange rates for a specific year for a list of currencies.</returns>
-	Task<AnnualAverageRatesModel> GetAnnualAverageRates(int year, IEnumerable<string> baseCurrencyIsoCodes,
-		string currencyIsoCode, Language language = Language.En);
+	Task<AnnualAverageRatesModel> GetAnnualAverageRates(CancellationToken cancellationToken, int year, IEnumerable<string> baseCurrencyIsoCodes, string currencyIsoCode, Language language = Language.En);
 
 	/// <summary>
 	/// Returns the daily exchange rates of a currency for a specific date range.
 	/// </summary>
+	/// <param name="cancellationToken">A cancellation token that can be used to receive notice of cancellation.</param>
 	/// <param name="startDate">The start date of the range for the exchange rates.</param>
 	/// <param name="endDate">The end date of the range for the exchange rates.</param>
 	/// <param name="baseCurrencyIsoCode">The ISO 4217 code of the required currency (case insensitive).</param>
 	/// <param name="currencyIsoCode">The ISO 4217 code of the reference currency ("EUR", "USD" or "ITL", case insensitive).</param>
 	/// <param name="language">The language of the returned data.</param>
 	/// <returns>A task that represents the asynchronous operation. The task result contains the daily exchange rates of a currency for a specific date range.</returns>
-	Task<DailyTimeSeriesModel> GetDailyTimeSeries(DateTime startDate, DateTime endDate, string baseCurrencyIsoCode,
-		string currencyIsoCode, Language language = Language.En);
+	Task<DailyTimeSeriesModel> GetDailyTimeSeries(CancellationToken cancellationToken, DateTime startDate, DateTime endDate, string baseCurrencyIsoCode, string currencyIsoCode, Language language = Language.En);
 
 	/// <summary>
 	/// Returns the monthly average exchange rates of a currency for a specific month range.
 	/// </summary>
+	/// <param name="cancellationToken">A cancellation token that can be used to receive notice of cancellation.</param>
 	/// <param name="startMonth">The start month of the range for the exchange rates (1-12).</param>
 	/// <param name="startYear">The start year of the range for the exchange rates.</param>
 	/// <param name="endMonth">The end month of the range for the exchange rates (1-12).</param>
@@ -126,25 +131,25 @@ public interface IExchangeRatesClient
 	/// <param name="currencyIsoCode">The ISO 4217 code of the reference currency ("EUR", "USD" or "ITL", case insensitive).</param>
 	/// <param name="language">The language of the returned data.</param>
 	/// <returns>A task that represents the asynchronous operation. The task result contains the monthly average exchange rates of a currency for a specific month range.</returns>
-	Task<MonthlyTimeSeriesModel> GetMonthlyTimeSeries(int startMonth, int startYear, int endMonth, int endYear,
-		string baseCurrencyIsoCode, string currencyIsoCode, Language language = Language.En);
+	Task<MonthlyTimeSeriesModel> GetMonthlyTimeSeries(CancellationToken cancellationToken, int startMonth, int startYear, int endMonth, int endYear, string baseCurrencyIsoCode, string currencyIsoCode, Language language = Language.En);
 
 	/// <summary>
 	/// Returns the annual average exchange rates of a currency for a specific year range.
 	/// </summary>
+	/// <param name="cancellationToken">A cancellation token that can be used to receive notice of cancellation.</param>
 	/// <param name="startYear">The start year of the range for the exchange rates.</param>
 	/// <param name="endYear">The end year of the range for the exchange rates.</param>
 	/// <param name="baseCurrencyIsoCode">The ISO 4217 code of the required currency (case insensitive).</param>
 	/// <param name="currencyIsoCode">The ISO 4217 code of the reference currency ("EUR", "USD" or "ITL", case insensitive).</param>
 	/// <param name="language">The language of the returned data.</param>
 	/// <returns>A task that represents the asynchronous operation. The task result contains the annual average exchange rates of a currency for a specific year range.</returns>
-	Task<AnnualTimeSeriesModel> GetAnnualTimeSeries(int startYear, int endYear, string baseCurrencyIsoCode,
-		string currencyIsoCode, Language language = Language.En);
+	Task<AnnualTimeSeriesModel> GetAnnualTimeSeries(CancellationToken cancellationToken, int startYear, int endYear, string baseCurrencyIsoCode, string currencyIsoCode, Language language = Language.En);
 
 	/// <summary>
 	/// Returns the list of all the available currencies.
 	/// </summary>
+	/// <param name="cancellationToken">A cancellation token that can be used to receive notice of cancellation.</param>
 	/// <param name="language">The language of the returned data.</param>
 	/// <returns>A task that represents the asynchronous operation. The task result contains the list of all the available currencies.</returns>
-	Task<CurrenciesModel> GetCurrencies(Language language = Language.En);
+	Task<CurrenciesModel> GetCurrencies(CancellationToken cancellationToken, Language language = Language.En);
 }
